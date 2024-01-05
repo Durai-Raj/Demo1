574b5afeb65631d41d937f0e95c8e6686518b458
Durai-Raj <57631023+Durai-Raj@users.noreply.github.com>
2024-01-05 15:19:34 +0530

    updated discount percentage

--- capstone 1/capstoneservicebase/capstoneservicebase/src/main/java/capstoneservice/CapstoneServiceImpl.java
+++ capstone 1/capstoneservicebase/capstoneservicebase/src/main/java/capstoneservice/CapstoneServiceImpl.java
@@ -20,7 +20,7 @@ public class CapstoneServiceImpl implements CapstoneServiceInterface {
             case "Laptop": {
                 switch (strDate) {
                     case "Tuesday","Thursday","Saturday","Sunday": {
-                        discount = (price * (7.5 / 100));
+                        discount = (price * (8.5 / 100));
                         break;
                     }
                 }
